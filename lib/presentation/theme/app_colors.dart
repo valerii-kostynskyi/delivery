c3efa9e70c03493f8ba30e0287c7259f96f4c5f4
Valerii Kostinskiy <valerii.kostynskyi@gmail.com>
2024-11-12 12:36:40 +0200

    change colors

--- lib/presentation/theme/app_colors.dart
+++ lib/presentation/theme/app_colors.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color white = Color(0xFFFFFFFF);
+  static const Color white = Color(0xFFF9FAFB);
   static const Color error = Color(0xFFF35625);
-  static const Color lightRed = Color(0xFFFDDDD3);
-  static const Color alert = Color(0xFFC2451E);
+  static const Color lightRed = Color(0xFFFDE0DB);
+  static const Color alert = Color.fromARGB(255, 246, 91, 56);
 
-  static const Color black = Color(0xFF353B41);
-  static const Color hint = Color(0xFF8593A3);
-  static const Color gray = Color(0xFF6A7682);
-  static const Color progressGray = Color(0xFFDEE4EB);
-  static const Color lightGray = Color(0xFFEFF2F5);
+  static const Color black = Color(0xFF2E3A42);
+  static const Color hint = Color(0xFF8A99A6);
+  static const Color gray = Color(0xFF707B85);
+  static const Color progressGray = Color(0xFFD8DEE5);
+  static const Color lightGray = Color(0xFFEAF0F4);
 
-  static const Color brown = Color(0xFF995200);
-  static const Color lightBrown = Color(0xFFFFF7E5);
+  static const Color brown = Color.fromARGB(255, 89, 44, 16);
+  static const Color lightBrown = Color(0xFFFFF3E6);
 
-  static const Color yellowLight = Color(0xFFFFEECC);
-  static const Color orange = Color(0xFFCC7A00);
+  static const Color yellowLight = Color(0xFFFFE7B8);
+  static const Color orange = Color(0xFFB86C30);
 
-  static const Color green = Color(0xFF20A17F);
-  static const Color lightGreen = Color(0xFFD4F4EC);
-  static const Color backgroundGreen = Color(0xFFE6FAF4);
+  static const Color green = Color.fromARGB(255, 105, 31, 145);
+  static const Color lightGreen = Color.fromARGB(255, 232, 190, 255);
+  static const Color backgroundGreen = Color(0xFFE1F7F0);
 
-  static const Color blue = Color(0xFF3F4EC4);
-  static const Color lightBlue = Color(0xFFEEF0FD);
-  static const Color scaffoldBackgroundColor = Color(0xFFF4F7FA);
+  static const Color blue = Color(0xFF3A47B8);
+  static const Color lightBlue = Color(0xFFE5E9FB);
+  static const Color scaffoldBackgroundColor = Color(0xFFF3F6F9);
 }
